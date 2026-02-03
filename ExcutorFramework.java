ad5ee7ab06225b0572724cab9361a247b33249a9
saurabh singh <raghuvanshisaurabh1729@gmail.com>
2026-02-03 07:41:39 +0530

    created new file

---
+++ ExcutorFramework.java
@@ -0,0 +1,19 @@
+package thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExcutorFramework {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for(int i = 0; i<=10;i++){
+             final int num = i;
+            executor.submit( () ->{
+                      System.out.println(num+" "+"Thread:"+num*num);
+            }
+            );
+        }
+        executor.shutdown();
+    }
+}
