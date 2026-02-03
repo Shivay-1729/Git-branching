f303a06b246ea5686428303100d610c6133e1d6c
saurabh singh <raghuvanshisaurabh1729@gmail.com>
2026-02-03 08:53:49 +0530

    deleted mycallable class

--- ExcutorFramework.java
+++ ExcutorFramework.java
@@ -4,22 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class myCallable implements java.util.concurrent.Callable<Integer> {
-    private int num;
 
-    public myCallable(int num) {
-        this.num = num;
-    }
-
-    @Override
-    public Integer call() throws Exception {
-        return num * num;
-    }
-}
 public class ExcutorFramework {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(5);
-   
+    
         for(int i = 0; i<=10;i++){
              final int num = i;
             executor.submit( () ->{
